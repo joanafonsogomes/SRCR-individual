aef2ffed131ddf3de67c96cc277448cf947e1159
joanafonsogomes <afonnso.gommes@gmail.com>
2021-06-05 19:03:15 +0100

    updts

--- indiv.pl
+++ indiv.pl
@@ -5,6 +5,7 @@
 :- set_prolog_flag( single_var_warnings,off ).
 :- set_prolog_flag( unknown,fail ).
 :- set_prolog_flag(toplevel_print_options,[quoted(true), portrayed(true), max_depth(0)]). 
+:- set_prolog_flag(stack_limit, 4_294_967_296).
 %-----------------------------------------------
 :- op( 900,xfy,'::' ).
 :- dynamic '-'/1.
@@ -19,7 +20,7 @@
 %-----------------------------------------------
 
 %-----------------------------------------------
-% Extensao do meta-predicado nao: Questao -> {V,F}
+% -- Auxiliares --
 
 nao( Questao ) :-
     Questao, !, fail. 
@@ -29,21 +30,43 @@ membro(X, [X|_]).
 membro(X, [_|Xs]):-
 	membro(X, Xs).
 
-solucoes( X,Y,Z ) :-findall( X,Y,Z ).
+% Concatena listas 
+concat(List1, List2, Result):-
+   append(List1, List2, Result).
+
+% Comprimento de uma lista
+compLista([H],R) :- R=1.
+compLista([H|T],R) :- compLista(T,O), R is O + 1.
+
+solucoes( X,Y,Z ) :- findall( X,Y,Z ).
 
 %-----------------------------------------------
 
-% Funcao que verifica se dois nodos sao adjacente
+% Verifica se dois nodos sao adjacente
 adjacente(X,Y,grafo(_,L_arestas)) :- member(aresta(X,Y),L_arestas).
-adjacente(X,Y,grafo(_,L_arestas)) :- member(aresta(Y,X),L_arestas).
 
-% Concatena listas 
-% concat(List1, List2, Result):-
-%   append(List1, List2, Result).
+% Calcula a distancia entre dois nodos
+distancia(Lat1,Lon1,Lat2,Lon2,D):- N is sqrt((Lat2-Lat1)^2+(Lon2-Lon1)^2),N=D.
 
-% Comprimento de uma lista
-% compLista([H],R) :- R=1.
-% compLista([H|T],R) :- compLista(T,O), R is O + 1.
+% myprog(X,Y) :- g(G), adjacente(X,Y,G).
+
+% caminho(G,X,Y,P) :- caminho1(G,X,[Y],P).
+% caminho1(_,X,[X|P1],[X|P1]).
+% caminho1(G,X,[Y|P1],P) :- 
+%   adjacente(Z,Y,G), \+ memberchk(Z,[Y|P1]), caminho1(G,X,[Z,Y|P1],P). 
+
+caminho(G,X,Y,C):-
+    caminhoAux(G,X,[Y],C).
+
+caminhoAux(_,X,[X|T],[X|T]).
+caminhoAux(G,X,[Y|T],P) :-
+    adjacente(Prox_nodo,Y,G), nao(membro(Prox_nodo,[Y|T])), caminhoAux(G,X,[Prox_nodo,Y|T],P).
+
+% TESTES
+% myprog(X,Y) :- g(G), adjacente(X,Y,G).
+% myTestProg(ponto(Lat1,Lon1,_,_,_,_,_,_,_),ponto(Lat2,Lon2,_,_,_,_,_,_,_),D) :- distancia(Lat1,Lon1,Lat2,Lon2,D).
+myTestProg(X,Y,C) :- g(G), caminho(G,X,Y,C).
+
 
 
 
